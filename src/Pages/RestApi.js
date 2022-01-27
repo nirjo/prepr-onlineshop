65a1a81d8e357733d94185699d6c21b51074f23a
nirjo <nirmalraj.joseph86@gmail.com>
2022-01-27 08:52:00 +0530

    Adding images to cards

--- src/Pages/RestApi.js
+++ src/Pages/RestApi.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from "react";
 // import axios from "axios";
-import { Container , Col, Row, Card , CardGroup ,FormControl } from "react-bootstrap";
-//  import 
-//  img1 from '../components/images/img1.jpg ';
+import {
+  Container,
+  Col,
+  Row,
+  Card,
+  CardGroup,
+  FormControl,
+} from "react-bootstrap";
+// import
+// img1 from '../components/images/img1.jpg ';
 
 const RestApi = () => {
   const [products, setProducts] = useState([]);
@@ -36,18 +43,18 @@ const RestApi = () => {
   return (
     <>
       <h2>Fetch Data with REST API</h2>
-      
+
       <ul>
         <FormControl
-              style={{ width: 500 }}
-              type="search"
-              placeholder="Search a product..."
-              className="m-auto"
-              aria-label="Search"
-            />
+          style={{ width: 500 }}
+          type="search"
+          placeholder="Search a product..."
+          className="m-auto"
+          aria-label="Search"
+        />{" "}
         {products.map((post) => (
           <li key={post.id}>
-          {/* <card style={{ width: '18rem' }}>
+            {/* <card style={{ width: '18rem' }}>
             <Card.Img variant="top" src="holder.js/100px180" />
 
             <Card.Title>{post.title}</Card.Title>
@@ -62,60 +69,66 @@ const RestApi = () => {
             </Card>
             </card> */}
 
-         <Container>
-           <Row>
-             <Col md={12}>
-             
-<CardGroup>
-  <Card className="mb-3" style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="img1/100px160" />
-    <Card.Body>
-      <Card.Title>{post.title}</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This content is a little bit longer.
-      </Card.Text>
-     <ul> category :{post.category}</ul>
-     <ul> Price: {post.price}</ul>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card className="mb-3" style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>{post.title}</Card.Title>
-      <Card.Text>
-        This card has supporting text below as a natural lead-in to additional
-        content.{' '}
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-  <Card className="mb-3" style={{ width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title>{post.title}</Card.Title>
-      <Card.Text>
-        This is a wider card with supporting text below as a natural lead-in to
-        additional content. This card has even longer content than the first to
-        show that equal height action.
-      </Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <small className="text-muted">Last updated 3 mins ago</small>
-    </Card.Footer>
-  </Card>
-</CardGroup>
-             </Col>
-           </Row>
-         </Container>   
- 
+            <Container>
+              <Row>
+                <Col md={12}>
+                  <CardGroup>
+                    <Card className="mb-3" style={{ width: "18rem" }}>
+                      <Card.Img variant="top" src="https://picsum.photos/id/1/200/300" />
+                      <Card.Body>
+                        <Card.Title>{post.title}</Card.Title>
+                        <Card.Text>
+                          This is a wider card with supporting text below as a
+                          natural lead-in to additional content. This content is
+                          a little bit longer.
+                        </Card.Text>
+                        <ul>category :{post.category}</ul>
+                        <ul>Price: {post.price}</ul>
+                      </Card.Body>
+                      <Card.Footer>
+                        <small className="text-muted">
+                          Last updated 3 mins ago
+                        </small>
+                      </Card.Footer>
+                    </Card>
+                    <Card className="mb-3" style={{ width: "18rem" }}>
+                      <Card.Img variant="top" src="https://picsum.photos/200/300" />
+                      <Card.Body>
+                        <Card.Title>{post.title}</Card.Title>
+                        <Card.Text>
+                          This card has supporting text below as a natural
+                          lead-in to additional content.{" "}
+                        </Card.Text>
+                      </Card.Body>
+                      <Card.Footer>
+                        <small className="text-muted">
+                          Last updated 3 mins ago
+                        </small>
+                      </Card.Footer>
+                    </Card>
+                    <Card className="mb-3" style={{ width: "18rem" }}>
+                      <Card.Img variant="top" src="https://picsum.photos/id/1/200/300" />
+                      <Card.Body>
+                        <Card.Title>{post.title}</Card.Title>
+                        <Card.Text>
+                          This is a wider card with supporting text below as a
+                          natural lead-in to additional content. This card has
+                          even longer content than the first to show that equal
+                          height action.
+                        </Card.Text>
+                      </Card.Body>
+                      <Card.Footer>
+                        <small className="text-muted">
+                          Last updated 3 mins ago
+                        </small>
+                      </Card.Footer>
+                    </Card>
+                  </CardGroup>
+                </Col>
+              </Row>
+            </Container>
           </li>
-        ))}
+        ))}{" "}
       </ul>
     </>
   );
